25cb21c93f7ceee49651c723e952feaa0b972b2d
Karen Hambro <khambro@gmail.com>
2015-10-14 15:34:02 -0700

    fixed bugs

--- app.rb
+++ app.rb
@@ -1,60 +1,34 @@
 require_relative 'bubble'
 
-#username = ''
-#body = ''
-#while username != 'exit' && body != 'exit'
-
 array_o_bubbles = []
 
+
+answer = "Y"
 puts "Would you like to post? (Y/N)"
 answer = gets.chomp.upcase
+
 while answer == "Y"
   puts "Username?"
   username = gets.chomp.downcase
   puts "What's on your mind #{username}?"
   body = gets.chomp
-  puts "#{username} says #{body}."
-  puts "Would you like to post? (Y/N)"
-  answer = gets.chomp.upcase
 
   hash_o_attrs = {username: username, body: body, created_at: Time.now}
 
-  b = Bubble.new(hash_o_attrs)
+  bubs = Bubble.new(hash_o_attrs)
 
-  array_o_bubbles << (b)
+  puts bubs.formatted_string
+
+  puts "Would you like to post? (Y/N)"
+  answer = gets.chomp.upcase
+
+  array_o_bubbles << bubs
 
 end
-
 
 puts "Goodbye!"
 
-
-#puts array_o_bubbles.inspect
-
-
-array_o_bubbles.each do |elements|
-  puts "Username: #{elements.username}"
-  puts "Bubble: #{elements.body}"
-  puts "Time: #{elements.created_at}"
+# to see entire array of postings
+array_o_bubbles.each do |bubble|
+  puts bubble.formatted_string
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Ask for a username
-#Ask for the body of a Bubble
-#Create a Bubble instance using that information (with created_at being Time.now)
-#Add the bubble instance to an Array
-#Print out the details of each bubble.
-#Start over at step 2.
--- bubble.rb
+++ bubble.rb
@@ -5,9 +5,13 @@ class Bubble
 
 
   def initialize(hash_o_attrs)
-    @username   = (hash_o_attrs)[:username]
+    @username   = hash_o_attrs[:username]
     @body       = hash_o_attrs[:body]
     @created_at = hash_o_attrs[:created_at]
+  end
+
+  def formatted_string
+    @formatted_string = "Username: #{@username}\nPost: #{@body}\nTime:#{@created_at}"
   end
 
 end
